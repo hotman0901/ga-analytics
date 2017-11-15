1bb973268fb88b0d15d8e61561c0d0ec9fa5a4fb
benny.hsieh <benny.hsieh@isuncloud.com>
2017-11-15 21:00:41 +0800

    update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,34 @@
 import React,{Component} from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import "./main.scss";
-export default class Header extends Component {
-
+class Header extends Component {
+    changeSelect()
+    {
+        if(this.select !== '')
+        {
+            this.props.history.push(`/${this.select.value}`)
+        }
+    }
     render() {
         return (
             <div className="header">
+                <select name="" id="" ref={(e)=>{this.select = e}} onChange={()=> this.changeSelect()}>
+                    <option value="">請選擇</option>
+                    <option value="page1">CIRCLE</option>
+                    <option value="page2">LINE</option>
+                    <option value="page3">COLUMN</option>
+                    <option value="page4">BAR</option>
+                    <option value="page5">TABLE</option>
+                    <option value="page7">Report Data</option>
+                </select>
+{/*                 
                 <Link to="/page1">CIRCLE  </Link>
                 <Link to="/page2">LINE  </Link>
                 <Link to="/page3">COLUMN  </Link> 
                 <Link to="/page4">BAR  </Link> 
                 <Link to="/page5">TABLE  </Link> 
                 <br/>
-                <Link to="/page7">Report Data  </Link> 
+                <Link to="/page7">Report Data  </Link>  */}
                 <br/>
                 <a href="https://developers.google.com/analytics/devguides/reporting/embed/v1/component-reference#datachart-options" target="_blank">doc </a>
                 <a href="https://developers.google.com/analytics/devguides/reporting/core/dimsmets" target="_blank">dimension </a>
@@ -23,3 +39,5 @@ export default class Header extends Component {
         );
     }
 }
+
+export default withRouter(Header)
